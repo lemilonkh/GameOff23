88a8b0b403b8c7e23786f9acd894ff79af62b962
Milan Gruner <milangruner@gmail.com>
2023-11-04 20:23:48 +0100

    Fix attack cooldown and typo in mushroom

--- scenes/enemies/mushroom/mushroom.gd
+++ scenes/enemies/mushroom/mushroom.gd
@@ -4,13 +4,13 @@ extends CharacterBody2D
 @export var speed := 20.0
 @export var gravity := 200.0
 @export var idle_distance_to_player := 40.0
-@export var projectile : PackedScene
+@export var projectile: PackedScene
 @export var wait_before_deactivate := 4.0
 @export var knockback_force := 80.0
 @export var knockback_decrease := 3.0
-@export var attack_wait_time := 20
+@export var attack_wait_time := 1.5
 
-@onready var _sprtie := $AnimatedSprite2D
+@onready var _sprite := $AnimatedSprite2D
 @onready var _state := $StateChart
 @onready var _move_states := $StateChart/Root/Movement
 @onready var _animations := $AnimationPlayer
@@ -18,13 +18,14 @@ extends CharacterBody2D
 @onready var _right_side := $Raycasts/RayCastRight
 @onready var _attack_spawner := $AttackSpawner
 
-var _player : Node2D
+var _player: Node2D
 var _active_time := Time.get_ticks_msec()
 var _player_in_range := false
 var _hit_effect := false
 var _hit_slowdown := 0.0
 var _wait_time := 0.0
 var _player_visible := false
+var _last_attack_time := 0.0
 
 
 func take_hit(amount: float, attacker: Node2D = null, direction: Vector2 = Vector2.ZERO, \
@@ -52,18 +53,18 @@ func _on_area_2d_body_exited(body):
 
 
 func _on_inactive_state_entered():
-	_sprtie.play("Inactive")
+	_sprite.play("Inactive")
 
 
 func _on_activate_state_entered():
-	_sprtie.play("Activate")
-	await _sprtie.animation_finished
+	_sprite.play("Activate")
+	await _sprite.animation_finished
 	_state.send_event("idle")
 
 
 func _on_deactivate_state_entered():
-	_sprtie.play("Deactivate")
-	await _sprtie.animation_finished
+	_sprite.play("Deactivate")
+	await _sprite.animation_finished
 	_state.send_event("inactive")
 
 
@@ -77,15 +78,15 @@ func _on_idle_state_processing(delta):
 
 
 func _on_walk_state_entered():
-	_sprtie.play("Walk")
-	await _sprtie.animation_finished
+	_sprite.play("Walk")
+	await _sprite.animation_finished
 	_state.send_event("idle")
 
 
 func _on_walk_state_physics_processing(delta):
 	if  (_player.position.x - self.position.x < 0 and !_left_side.is_colliding()) \
 		or (_player.position.x - self.position.x > 0 and !_right_side.is_colliding()):
-		_sprtie.stop()
+		_sprite.stop()
 		_state.send_event("idle")
 		return
 	self.velocity.x = speed if _player.position.x - self.position.x > 0 else -speed
@@ -96,8 +97,8 @@ func _on_walk_state_exited():
 
 
 func _on_dead_state_entered():
-	_sprtie.play("Die")
-	await _sprtie.animation_finished
+	_sprite.play("Die")
+	await _sprite.animation_finished
 	await get_tree().create_timer(3.0).timeout
 	_animations.play("death")
 	await _animations.animation_finished
@@ -111,7 +112,9 @@ func _on_hit_state_entered():
 
 
 func _on_passive_state_processing(delta):
-	if _player_visible and !(fmod(randf(), attack_wait_time)):
+	_last_attack_time += delta
+	if _player_visible and _last_attack_time > attack_wait_time:
+		_last_attack_time = 0
 		_state.send_event("attack")
 
 
@@ -148,7 +151,7 @@ func _physics_process(delta):
 	var state := str(_move_states._active_state).left(8)
 	if (state != "Inactive") and (state != "Deactiva") \
 		and (state != "Activate") and (state.left(4) != "Dead"):
-		_sprtie.scale.x = 1 if (_player.position.x - self.position.x < 0) else -1
+		_sprite.scale.x = 1 if (_player.position.x - self.position.x < 0) else -1
 		#$CollisionShape2D.position.x = -position.x if (_player.position.x - self.position.x < 0) else -1
 	
 	# Update player visibility
