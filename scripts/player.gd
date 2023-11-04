c215f780ab45686f9629ce43de9b3707914d7270
Milan Gruner <milangruner@gmail.com>
2023-11-04 09:51:06 +0100

    Add exports for player parameters

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -430.0
-const GLIDE_INITIAL_GRAVITY_FACTOR = 0.8
-const GLIDE_GRAVITY_FACTOR = 0.05
-const GLIDE_SPEED_FACTOR = 2.0
-const GLIDE_INERTIA = 0.94
-const GLIDE_DROP_DURATION = 0.3
+## Default movement speed
+@export_range(0, 1000) var speed := 300.0
+## How far up a jump goes
+@export_range(0, 1000) var jump_velocity := 430.0
+## Percent of previous velocity that remains each frame during normal movement
+@export_range(0, 1) var default_inertia := 0.1
+## Duration of initial drop for glide in seconds
+@export_range(0, 2) var glide_drop_duration := 0.2
+## How much you drop at the start of the glide
+@export_range(0, 1) var glide_initial_gravity_factor := 0.6
+## How much you drop during the rest of the glide
+@export_range(0, 1) var glide_gravity_factor := 0.05
+## Factor of increase of the current speed when starting a glide
+@export_range(0, 5) var glide_speed_factor := 1.5
+## Percent of previous velocity that remains each frame during glide
+@export_range(0, 1) var glide_inertia := 0.94
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,7 +24,7 @@ var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var state_chart: Node = $StateChart
 
 var gravity_factor := 1.0
-var inertia := 0.1
+var inertia := default_inertia
 var gravity_tween: Tween
 
 func _physics_process(delta: float) -> void:
@@ -23,10 +32,10 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = lerp(direction * SPEED, velocity.x, inertia)
+		velocity.x = lerp(direction * speed, velocity.x, inertia)
 	else:
 		velocity.x = lerp(0.0, velocity.x, inertia)
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.x = move_toward(velocity.x, 0, speed)
 
 	if signf(velocity.x) != 0:
 		sprite.flip_h = velocity.x < 0
@@ -58,7 +67,7 @@ func _input(event: InputEvent) -> void:
 
 func _on_jump_enabled_state_physics_processing(delta):
 	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -jump_velocity
 		state_chart.send_event("jump")
 
 func _on_animation_finished() -> void:
@@ -70,13 +79,13 @@ func _play_animation(animation: StringName) -> void:
 func _on_glide_state_entered() -> void:
 	if velocity.y < 0:
 		velocity.y = 0
-	velocity.x *= GLIDE_SPEED_FACTOR
-	inertia = GLIDE_INERTIA
-	gravity_factor = GLIDE_INITIAL_GRAVITY_FACTOR
+	velocity.x *= glide_speed_factor
+	inertia = glide_inertia
+	gravity_factor = glide_initial_gravity_factor
 	gravity_tween = create_tween().set_trans(Tween.TRANS_EXPO)
-	gravity_tween.tween_property(self, "gravity_factor", GLIDE_GRAVITY_FACTOR, GLIDE_DROP_DURATION)
+	gravity_tween.tween_property(self, "gravity_factor", glide_gravity_factor, glide_drop_duration)
 
 func _on_glide_state_exited() -> void:
 	gravity_tween.kill()
 	gravity_factor = 1.0
-	inertia = 0.0
+	inertia = default_inertia
