7ac638b8bf2d8b874c9dac3a2151e68b24bc6510
Milan Gruner <milangruner@gmail.com>
2023-11-04 16:35:12 +0100

    Implement shrooms bouncing in different directions

--- scenes/objects/bouncy_shroom.gd
+++ scenes/objects/bouncy_shroom.gd
@@ -1,10 +1,29 @@
 extends StaticBody2D
 
+enum Direction {
+	UP,
+	LEFT,
+	DOWN,
+	RIGHT,
+}
+
 @export var bounce_speed := 600.0
+@export var direction: Direction = Direction.UP
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	rotation = -direction * PI / 2
 
 func on_collision(collider: Object) -> void:
 	animation_player.play(&"bounce")
 	if collider is CharacterBody2D:
-		collider.velocity.y = -bounce_speed
+		match direction:
+			Direction.UP:
+				collider.velocity.y = -bounce_speed
+			Direction.DOWN:
+				collider.velocity.y = bounce_speed
+			Direction.LEFT:
+				collider.velocity.x = -bounce_speed
+			Direction.RIGHT:
+				collider.velocity.x = bounce_speed
