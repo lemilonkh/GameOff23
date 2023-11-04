25a17161713ad4fe28d5ae3e61e14189bc72c909
AlexHCC <alexander06088@yahoo.com>
2023-11-04 16:04:52 +0200

    Improves enemy

--- scenes/enemies/mushroom/mushroom.gd
+++ scenes/enemies/mushroom/mushroom.gd
@@ -1,31 +1,34 @@
 extends CharacterBody2D
 
-@export var health = 100
-@export var speed = 20
-@export var gravity = 200
-@export var idle_distance_to_player = 40
+@export var health := 100.0
+@export var speed := 20.0
+@export var gravity := 200.0
+@export var idle_distance_to_player := 40.0
 @export var projectile : PackedScene
-@export var wait_before_deactivate = 4
-@export var knockback_force = 80
-@export var knockback_decrease = 3
-@export var attack_wait_time = 20
+@export var wait_before_deactivate := 4.0
+@export var knockback_force := 80.0
+@export var knockback_decrease := 3.0
+@export var attack_wait_time := 20.0
 
-@onready var _sprtie = $AnimatedSprite2D
-@onready var _state = $StateChart
-@onready var _move_states = $StateChart/Root/Movement
-@onready var _animations = $AnimationPlayer
-@onready var _left_side = $Raycasts/RayCastLeft
-@onready var _right_side = $Raycasts/RayCastRight
-@onready var _attack_spawner = $AttackSpawner
+@onready var _sprtie := $AnimatedSprite2D
+@onready var _state := $StateChart
+@onready var _move_states := $StateChart/Root/Movement
+@onready var _animations := $AnimationPlayer
+@onready var _left_side := $Raycasts/RayCastLeft
+@onready var _right_side := $Raycasts/RayCastRight
+@onready var _attack_spawner := $AttackSpawner
 
 var _player : Node2D
-var _active_time = Time.get_ticks_msec()
-var _player_in_range = false
-var _hit_effect = false
-var _hit_slowdown = 0.0
+var _active_time := Time.get_ticks_msec()
+var _player_in_range := false
+var _hit_effect := false
+var _hit_slowdown := 0.0
+var _wait_time := 0.0
+var _player_visible := false
 
 
-func hit(amount):
+func take_hit(amount: float, attacker: Node2D = null, direction: Vector2 = Vector2.ZERO, \
+	knockback_force: float = 80) -> void:
 	health -= amount
 	_state.send_event("hit")
 	if health <= 0:
@@ -46,7 +49,6 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body == _player:
 		_player_in_range = false
-		_active_time = Time.get_ticks_msec()
 
 
 func _on_inactive_state_entered():
@@ -66,7 +68,6 @@ func _on_deactivate_state_entered():
 
 
 func _on_idle_state_processing(delta):
-	# TODO: Add random pauses
 	if  (_player.position.x - self.position.x < 0 and !_left_side.is_colliding()) \
 		or (_player.position.x - self.position.x > 0 and !_right_side.is_colliding()):
 		# TODO: Add smarter indicators when to walk towards the player
@@ -110,22 +111,15 @@ func _on_hit_state_entered():
 
 
 func _on_passive_state_processing(delta):
-	var space_state = get_world_2d().direct_space_state
-	var from = global_transform.origin
-	var to = _player.global_transform.origin
-	var query = PhysicsRayQueryParameters2D.create(from, to)
-	var result = space_state.intersect_ray(query)
-	
-	if result:
-		if result["collider"] == _player:
-			if !(randi() % attack_wait_time):
-				_state.send_event("attack")
+	if _player_visible and !(randi() % attack_wait_time):
+		_state.send_event("attack")
 
 
 func _on_attack_state_entered():
-	var attack = projectile.instantiate()
+	var attack := projectile.instantiate()
 	_attack_spawner.add_child(attack)
 	attack.look_at(_player.global_transform.origin)
+	attack.enemy = self
 	_state.send_event("passive")
 
 
@@ -137,25 +131,32 @@ func _physics_process(delta):
 	_hit_slowdown = max(0, _hit_slowdown - knockback_decrease)
 	
 	# Calculate final velocty
-	var direction = 1 if (_player.position.x - self.position.x < 0) else -1
+	var direction := 1 if (_player.position.x - self.position.x < 0) else -1
 	velocity.x += _hit_slowdown*direction
 	velocity.y = gravity
 	move_and_slide()
 	velocity = Vector2.ZERO
 	
 	# When to deactivate
+	if (_player_visible and abs((_player.global_position - self.global_position).length()) \
+		<= 300) or (not _player_visible and _player_in_range):
+		_active_time = Time.get_ticks_msec()
 	if Time.get_ticks_msec() - _active_time > wait_before_deactivate*1000:
-		if (!_player_in_range):  # TODO: Add more indicators when to disable
-			_state.send_event("deactivate")
+		_state.send_event("deactivate")
 	
 	# Change look direction
-	var state = str(_move_states._active_state).left(8)
+	var state := str(_move_states._active_state).left(8)
 	if (state != "Inactive") and (state != "Deactiva") \
 		and (state != "Activate") and (state.left(4) != "Dead"):
 		_sprtie.scale.x = 1 if (_player.position.x - self.position.x < 0) else -1
+		#$CollisionShape2D.position.x = -position.x if (_player.position.x - self.position.x < 0) else -1
 	
-	# TODO: Debug - remove
-	if Input.is_action_just_pressed("attack"):
-		await get_tree().create_timer(0.4).timeout
-		hit(35)
+	# Update player visibility
+	var space_state := get_world_2d().direct_space_state
+	var from := global_transform.origin
+	var to := _player.global_transform.origin
+	var query := PhysicsRayQueryParameters2D.create(from, to)
+	var result := space_state.intersect_ray(query)
+	if result:
+		_player_visible = (result["collider"] == _player)
 
--- scenes/enemies/mushroom/projectile.gd
+++ scenes/enemies/mushroom/projectile.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
-@export var speed = 10
-@export var life_time = 1000
+@export var speed := 10.0
+@export var hit_amount := 10.0
+@export var knockback_force := -1.0
+@export var life_time := 1.0
 
-@onready var _trail = $Trail
-@onready var _explosion = $Explosion
+@onready var _trail := $Trail
+@onready var _explosion := $Explosion
 
-var _start_time
-var _hit = false
-
+var _start_time : float
+var _hit := false
+var enemy :Node2D
 
 func _ready():
 	_start_time = Time.get_ticks_msec()
@@ -20,12 +22,21 @@ func _physics_process(delta):
 		return
 	var move_direction = Vector2(speed, 0)
 	self.position += move_direction.rotated(self.rotation)
-	if Time.get_ticks_msec() - _start_time > life_time:
+	if Time.get_ticks_msec() - _start_time > (life_time*1000):
 		self.queue_free()
 
 
 func _on_body_entered(body):
-	# TODO: Callback player hit function
+	# Inform player of hit
+	if body.is_in_group("Player"):
+		if body.has_method("take_hit"):
+			var direction :Vector2 = (body.global_position - self.global_position).normalized()
+			if knockback_force >= 0:
+				body.take_hit(hit_amount, enemy, direction, knockback_force)
+			else:
+				body.take_hit(hit_amount, enemy, direction)
+	
+	# Play hit animation
 	_hit = true
 	_trail.emitting = false
 	await get_tree().create_timer(0.03).timeout
