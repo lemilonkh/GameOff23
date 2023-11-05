044d6989ab1e9c611fcb73ba93ed95cc37f354f9
Milan Gruner <milangruner@gmail.com>
2023-11-05 11:08:17 +0100

    Create a generic hurtbox script

---
+++ scripts/hurtbox.gd
@@ -0,0 +1,25 @@
+extends Area2D
+class_name Hurtbox
+
+signal hit(body: Node2D)
+
+@export_range(0, 50) var damage := 1.0
+@export var is_enabled := false: set = set_enabled
+
+func set_enabled(value: bool) -> void:
+	if is_enabled != value:
+		for body in get_overlapping_bodies():
+			deal_damage(body)
+
+	is_enabled = value
+
+func _on_body_entered(body: Node2D) -> void:
+	if !is_enabled:
+		return
+	deal_damage(body)
+
+func deal_damage(body: Node2D) -> void:
+	if body.has_method("take_hit"):
+		var direction := global_position.direction_to(body.global_position)
+		body.take_hit(damage, self, direction)
+		hit.emit(body)
