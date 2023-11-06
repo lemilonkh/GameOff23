cec8ea1d6ec7be836bcbda0d4bf1b9b1ad6f1d76
Milan Gruner <milangruner@gmail.com>
2023-11-06 10:59:55 +0100

    Use an exponential transition for the music filter tween

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -3,7 +3,7 @@ extends Node
 const TARGET_RESONANCE = 0.33
 
 func animate_filter(bus_name: StringName, duration: float = 1.0) -> void:
-	var tween := create_tween().set_trans(Tween.TRANS_QUAD)
+	var tween := create_tween().set_trans(Tween.TRANS_EXPO)
 	var bus_id := AudioServer.get_bus_index(bus_name)
 	var effect: AudioEffectBandPassFilter = AudioServer.get_bus_effect(bus_id, 0)
 	effect.resonance = 0
