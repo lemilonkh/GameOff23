38c0b7584ae07440723f094f1efc70ebee5adbba
Milan Gruner <milangruner@gmail.com>
2023-11-09 11:40:50 +0100

    Make fade in for take damage audio filter faster for immediate feedback

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -7,5 +7,6 @@ func animate_filter(bus_name: StringName, duration: float = 1.0) -> void:
 	var bus_id := AudioServer.get_bus_index(bus_name)
 	var effect: AudioEffectBandPassFilter = AudioServer.get_bus_effect(bus_id, 0)
 	effect.resonance = 0
-	tween.tween_property(effect, "resonance", TARGET_RESONANCE, duration / 2)
-	tween.tween_property(effect, "resonance", 0, duration / 2)
+	var start_duration := duration / 4
+	tween.tween_property(effect, "resonance", TARGET_RESONANCE, start_duration)
+	tween.tween_property(effect, "resonance", 0, duration - start_duration)
