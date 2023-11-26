47f60b2e94eaf84191c8398392eb2ab84cc9cb74
Milan Gruner <milangruner@gmail.com>
2023-11-26 17:43:31 +0100

    Make hurtbox also deal damage to areas

--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -7,10 +7,16 @@ signal tile_hit(tilemap: TileMap)
 @export_range(0, 50) var damage := 1.0
 @export var is_enabled := false: set = set_enabled
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	area_entered.connect(_on_area_entered)
+
 func set_enabled(value: bool) -> void:
 	if is_enabled != value and is_enabled:
 		for body in get_overlapping_bodies():
 			deal_damage(body)
+		for area in get_overlapping_areas():
+			deal_damage(area)
 
 	is_enabled = value
 
@@ -18,6 +24,11 @@ func _on_body_entered(body: Node2D) -> void:
 	if !is_enabled:
 		return
 	deal_damage(body)
+
+func _on_area_entered(area: Area2D) -> void:
+	if !is_enabled:
+		return
+	deal_damage(area)
 
 func deal_damage(body: Node2D) -> void:
 	if body.has_method("take_hit"):
