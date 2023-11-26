53238f70eac66dfa95df7fa8642b941bd2377576
Milan Gruner <milangruner@gmail.com>
2023-11-26 22:40:31 +0100

    Adjust ability scale tween after dragon boss and fix potential crash

--- maps/scripts/jungle_boss1.gd
+++ maps/scripts/jungle_boss1.gd
@@ -13,12 +13,15 @@ func _ready() -> void:
 	tile_map.set_layer_enabled(2, true)
 
 func _on_dragon_death() -> void:
+	if !is_instance_valid(ability_scale):
+		printerr("Missing grapple ability scale!")
+		return
+	
 	ability_scale.global_position = dragon.global_position + Vector2.UP * dragon_mouth_offset
-	prints("scale pos", ability_scale.global_position)
-	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
+	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
 	tween.tween_property(ability_scale, "modulate", Color.WHITE, 2.0)
 	var target_pos := Vector2(ability_scale.global_position.x, above_floor_height)
-	tween.tween_property(ability_scale, "global_position", target_pos, 3.0)
+	tween.tween_property(ability_scale, "global_position", target_pos, 4.0)
 	tween.tween_callback(func():
 		ability_scale.monitoring = true
 	)
