d44d6e040583c21a9970f81bc522a53633862655
Milan Gruner <milangruner@gmail.com>
2023-11-28 21:47:51 +0100

    Pass more context variables into dialogue
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- scenes/npcs/npc.gd
+++ scenes/npcs/npc.gd
@@ -6,12 +6,17 @@ extends Area2D
 @export var npc_name: String
 
 var dialogue: DialogueResource
+var has_finished_intro := false
 
 func _ready() -> void:
 	dialogue = load(dialogue_file)
 
 func _on_body_entered(body: Node2D) -> void:
-	Game.get_singleton().start_dialogue(dialogue, initial_title, [self])
+	Game.get_singleton().start_dialogue(dialogue, initial_title, [{
+		npc = self,
+		scene = owner,
+		game = Game.get_singleton()
+	}])
 
 func _on_body_exited(body: Node2D) -> void:
 	Game.get_singleton().exit_dialogue()
