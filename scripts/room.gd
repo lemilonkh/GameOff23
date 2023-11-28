f605e9984bd653cc4c64a3003633324a74ec22e9
Milan Gruner <milangruner@gmail.com>
2023-11-28 12:54:03 +0100

    Add room script that exports filenames for music, drums and ambience

---
+++ scripts/room.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name Room
+
+@export_file("*.ogg", "*.wav", "*.mp3") var music_file: String = "res://music/jungle_music.ogg"
+@export_file("*.ogg", "*.wav", "*.mp3") var drums_file: String = "res://music/jungle_drums.ogg"
+@export_file("*.ogg", "*.wav", "*.mp3") var ambience_file: String = "res://sounds/Ambience/Ambience_Jungle.ogg"
