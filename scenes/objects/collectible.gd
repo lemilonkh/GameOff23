81ec744215106d698b1bc167d4f24bc813f94519
Milan Gruner <milangruner@gmail.com>
2023-11-28 13:44:04 +0100

    Implement collectible object for scale shards

---
+++ scenes/objects/collectible.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@export var move_speed := 120.0
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+var target_node: Node2D
+
+func _ready() -> void:
+	# Register as storable with a marker. The marker will appear on the map when the orb is first discovered (i.e. get instantiated).
+	MetSys.register_storable_object_with_marker(self)
+
+func _process(delta: float) -> void:
+	if not target_node:
+		return
+
+	global_position = global_position.move_toward(target_node.global_position, move_speed * delta)
+	if global_position.distance_to(target_node.global_position) < 1:
+		set_process(false)
+		Game.get_singleton().collectibles += 1
+		MetSys.store_object(self)
+		var tween := create_tween().set_trans(Tween.TRANS_SPRING)
+		tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.3)
+		tween.tween_callback(queue_free)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		target_node = body
