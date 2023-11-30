49f899fae0f8d918356de5a023c74bfd20f07499
Milan Gruner <milangruner@gmail.com>
2023-11-30 11:50:35 +0100

    Make head hits to dragon count double

--- scenes/enemies/dragon/hitbox.gd
+++ scenes/enemies/dragon/hitbox.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var hurt_color := Color("#ce2d3c")
+@export var damage_factor := 1.0
 
 @onready var sprite: Sprite2D = get_node_or_null(^"Sprite2D")
 
@@ -9,7 +10,7 @@ func _ready() -> void:
 		sprite = $Head
 
 func take_hit(amount: float, attacker: Node2D = null, direction: Vector2 = Vector2.ZERO, knockback_force: float = 0) -> void:
-	owner.take_hit(amount, attacker, direction, knockback_force)
+	owner.take_hit(amount * damage_factor, attacker, direction, knockback_force)
 	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
 	tween.tween_property(sprite, "modulate", hurt_color, 0.3)
 	tween.tween_interval(0.1)
